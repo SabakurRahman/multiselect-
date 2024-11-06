62f13574a2503d8b411a89d115264fff11d7daa5
SabakurRahman <sabakur12360@gmail.com>
2024-11-06 22:35:02 +0600

    colour generate

--- src/components/colour/index.jsx
+++ src/components/colour/index.jsx
@@ -1,10 +1,52 @@
+import { useState } from "react";
 import "./style.css";
 export default function Colour() {
+  const [hex, setHex] = useState("#000000");
+  const [rgb, setRgb] = useState("rgb(0,0,0)");
+  const [bgColour, setBgColour] = useState("rgb(0,0,0)");
+
+  const handleHexColour = () => {
+    const newHex = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    setHex(newHex);
+    setBgColour(newHex);
+  };
+  const handleRgbColour = () => {
+    const newRgb =
+      "rgb(" +
+      Math.floor(Math.random() * 255) +
+      "," +
+      Math.floor(Math.random() * 255) +
+      "," +
+      Math.floor(Math.random() * 255) +
+      ")";
+    setRgb(newRgb);
+    setBgColour(newRgb);
+  };
+  const handleRandomColour = () => {
+    const newHex = "#" + Math.floor(Math.random() * 16777215).toString(16);
+
+    const newRgb =
+      "rgb(" +
+      Math.floor(Math.random() * 255) +
+      "," +
+      Math.floor(Math.random() * 255) +
+      "," +
+      Math.floor(Math.random() * 255) +
+      ")";
+    setHex(newHex);
+    setRgb(newRgb);
+    setBgColour(newRgb);
+  };
+  console.log(hex, rgb);
+
   return (
-    <div className="div1">
-      <button>Click up To HEX</button>
-      <button>Click Up to RGB</button>
-      <button>Rendom</button>
-    </div>
+    <>
+      <button onClick={handleHexColour}>Click up To HEX</button>
+      <button onClick={handleRgbColour}>Click Up to RGB</button>
+      <button onClick={handleRandomColour}>Rendom</button>
+      <div className="div1" style={{ backgroundColor: bgColour }}>
+        Hex: {hex} | RGB: {rgb}
+      </div>
+    </>
   );
 }
